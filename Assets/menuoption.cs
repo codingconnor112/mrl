aa45a4e28470384b97104c957ca6949e6ca612e0
codingconnor112 <68755068+codingconnor112@users.noreply.github.com>
2025-06-25 11:09:16 -0700

    character names and idle anim

--- Assets/menuoption.cs
+++ Assets/menuoption.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class menuoption : MonoBehaviour
 {
-    /*public Sprite activated;
-    public Sprite deactivated;*/
+    public Sprite activated;
+    public Sprite deactivated;
+    public Image selfimage;
     public bool selected;
+    public bool mouse_selected=false;
 
     public GameObject up;
     public GameObject down;
     public GameObject left;
     public GameObject right;
+
+    public GameObject dropoff;
+    public string dropoffclass;
+    public string classfunction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        selfimage = gameObject.GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -54,5 +61,42 @@ public class menuoption : MonoBehaviour
                 left.GetComponent<menuoption>().selected = true;
             }
         }
+
+        if (mouse_selected || selected)
+        {
+            selfimage.sprite = activated;
+        }
+        else
+        {
+            selfimage.sprite = deactivated;
+        }
+
+        if (Input.GetAxis("Submit")==1)
+        {
+            if (selected)
+            {
+                dropoff.GetComponent(dropoffclass).SendMessage(classfunction);
+            }
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (mouse_selected)
+            {
+                dropoff.GetComponent(dropoffclass).SendMessage(classfunction);
+            }
+        }
+    }
+    private void OnMouseEnter()
+    {
+        mouse_selected = true;
+    }
+    private void OnMouseExit()
+    {
+        mouse_selected = false;
+    }
+    private void OnDisable()
+    {
+        selected = false;
+        mouse_selected = false;
     }
 }
